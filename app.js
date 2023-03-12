ade829cbe366cb8b56cb882663b435227994b388
ShishKirill <119962653+ShishKirill@users.noreply.github.com>
2023-03-12 12:28:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 let expectedWords = ["rock", "paper", "scissors"]
 let gameRound = 0;
-let playerPoints = 0;
-let computerPoints = 0;
-let playerChoice = "";
 let computerChoice = "";
 let playAgain = "";
 
 game();
 
-function getChoices (){
-        playerChoice = prompt("Rock, paper or scissors?");
-        computerChoice = expectedWords[Math.floor(expectedWords.length * Math.random())];
-        pointScore();
-}
-
-function fixUserChoice() {
-    if(playerChoice != null) {
+function getChoices() {
+    playerChoice = prompt("Rock, paper or scissors?");
+    if (playerChoice != null) {
         playerChoice = (playerChoice.replace(/[^a-zA-Z]/g, '')).toLowerCase();
-    } else {
-        getNull();
     }
+    computerChoice = expectedWords[Math.floor(expectedWords.length * Math.random())];
+    pointScore();
 }
 
-function pointScore () {
+function pointScore() {
+
     if (playerChoice === computerChoice) {
         console.log(`It's a tie!`);
     } else if (playerChoice === "paper" && computerChoice === "rock" || playerChoice === "scissors" && computerChoice === "paper" || playerChoice === "rock" && computerChoice === "scissors") {
@@ -31,38 +24,38 @@ function pointScore () {
     } else if (playerChoice === "rock" && computerChoice === "paper" || playerChoice === "paper" && computerChoice === "scissors" || playerChoice === "scissors" && computerChoice === "rock") {
         console.log(`Computer won!`);
         computerPoints++;;
-    } else if( playerChoice == null){
+    } else if (playerChoice == null) {
         getNull();
     } else {
-        alert ("Please choose again.");
+        alert("Please choose again.");
         getChoices();
     }
 }
 
-function displayConsoleScore () {
-    if (playerChoice != null){
-       console.log(`You: ${playerChoice} -- Computer: ${computerChoice}
-        You: ${playerPoints} -- Computer: ${computerPoints}`); 
-    }     
+function displayConsoleScore() {
+    if (playerChoice != null) {
+        console.log(`You: ${playerChoice} -- Computer: ${computerChoice}
+        You: ${playerPoints} -- Computer: ${computerPoints}`);
+    }
 }
 
-function displayAlertScore (){
+function displayAlertScore() {
     if (playerChoice != null) {
         alert(`You: ${playerChoice} -- Computer: ${computerChoice}
         You: ${playerPoints} -- Computer: ${computerPoints}`
-    )}
+        )
+    }
 }
 
-
-function getWhoWon (){
+function getWhoWon() {
     if (playerPoints > computerPoints && playerChoice != null) {
         console.log(`You won the game!`);
         alert("Well done! You won!");
-        
+
     } else if (computerPoints > playerPoints && playerChoice != null) {
         console.log(`Computer won the game. :(`);
         alert("Oh no, the computer won. :(");
-    } else if ( playerChoice == null) {
+    } else if (playerChoice == null) {
         getNull();
     } else {
         console.log(`It's a tie!`);
@@ -70,13 +63,13 @@ function getWhoWon (){
     }
 }
 
-function getPlayAgain(){
+function getPlayAgain() {
     playAgain = prompt("Do you want to play again? Yes or no?");
-    getPlayAgainValue ();
+    getPlayAgainValue();
 }
 
 
-function getPlayAgainValue () {
+function getPlayAgainValue() {
     fixPlayAgain();
     if (playAgain === "yes") {
         game();
@@ -90,7 +83,7 @@ function getPlayAgainValue () {
     }
 }
 
-function fixPlayAgain () {
+function fixPlayAgain() {
     if (playAgain != null) {
         playAgain = (playAgain.replace(/[^a-zA-Z]/g, '').toLowerCase());
     } else {
@@ -98,29 +91,30 @@ function fixPlayAgain () {
     }
 }
 
-function getNull (){
+function getNull() {
     return playerChoice = null;
 }
 
 
 function game() {
     playerChoice = "";
-        
+    playerPoints = 0;
+    computerPoints = 0;
     alert(`Hello! You are about to play the game Rock, Paper Scissors. These are the rules:
     1. You choose rock, paper or scissors. 
     2. The computer also chooses either rock, paper or scissors.
     3. Rock beats scissors, paper beats rock, and scissors beats paper.
     4. We will play for 5 rounds and the player with the most points wins!`)
 
-    for (i = 0; i < 5 && playerChoice != null; i++){
-        console.log(`Round ${i+1}`),
-        getChoices();
-        fixUserChoice();
-        displayConsoleScore(); 
-        displayAlertScore()
+    for (i = 0; i < 5 && playerChoice != null; i++) {
+        console.log(`Round ${i + 1}`),
+            getChoices();
+
+        displayConsoleScore();
+        displayAlertScore();
     };
     getWhoWon();
     getPlayAgain();
-    playerPoints = 0;
-    computerPoints = 0;
-}
+    console.log("If you want to play again, please reload the page!")
+};
+
